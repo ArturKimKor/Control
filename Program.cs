4ba870c45501559a6b79164d1001ec15deb09b9e
Artur Kim <artur.kim.kor@gmail.com>
2024-11-22 12:16:54 +0300

    Исправил некоторые ошибки

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿
+class Program
+{
     static void Main()
     {
-        Console.WriteLine("Введите элементы массива через запятую:" );
+        Console.WriteLine("Введите элементы массива через запятую:");
         string input = Console.ReadLine();
         string[] inputArray = input.Split(',');
+
         string[] resultArray = GetShortStrings(inputArray);
+
         Console.WriteLine($"Результат: [{string.Join(", ", resultArray)}]");
     }
+
     static string[] GetShortStrings(string[] array)
     {
         int count = 0;
@@ -15,10 +20,10 @@
             if (str.Trim().Length <= 3)
                 count++;
         }
-    }
+
         string[] result = new string[count];
         int index = 0;
-            // Заполняем новый массив строками длиной ≤ 3
+
         foreach (string str in array)
         {
             if (str.Trim().Length <= 3)
@@ -30,6 +35,7 @@
 
         return result;
     }
+}
 
 
 
